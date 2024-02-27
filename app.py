8ea0bc19700dd0bdb34b0b81b5bdaeab4baed92e
Amir <you@hobee.com>
2024-02-27 19:26:42 +0500

    HomeProject

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB.db'
+app.app_context().push()
+db = SQLAlchemy (app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float)
+    description = db.Column(db.String)
+    imageName = db.Column(db.String)
+
+
+
+@app.route("/")
+def home():
+    return render_template("homePage.html", products = products)
+
+@app.route("/product/<int:id>")
+def product(id):
+   return render_template("product.html", product = products[id - 1])
+    
