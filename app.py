abbedf2d46830307ae3a47101e5ecfce756013ef
Amir <you@hobee.com>
2024-02-28 17:58:29 +0500

    added styles to addproduct

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB.db'
 app.app_context().push()
 db = SQLAlchemy (app)
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)   
     name = db.Column(db.String, nullable=False)
-    price = db.Column(db.Float)
-    description = db.Column(db.String)
-    imageName = db.Column(db.String)
-
-
-
-@app.route("/")
+    price = db.Column(db.Float)   
+    description = db.Column(db.String)   
+    imageName = db.Column(db.String) 
+      
+@app.route("/")   
 def home():
+    products = Product.query.all()
     return render_template("homePage.html", products = products)
 
 @app.route("/product/<int:id>")
 def product(id):
-   return render_template("product.html", product = products[id - 1])
+    product = Product.query.get(id)
+    return render_template("product.html", product = product)
+
+@app.route("/addproduct", methods=['GET', 'POST'])
+def addproduct():
+    if request.method == 'GET':
+        return render_template ("addproduct.html")  
+    else:
+        return f"confirmed {request.form['name']}"  
     
+@app.route("/about")
+def about():
+    return "О нас"
